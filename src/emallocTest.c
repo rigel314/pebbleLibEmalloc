4660dcca39c8e5d97acfa8a76318140cc6acec59
Cody Creager <pi.rubiks@gmail.com>
2013-07-27 03:24:53 -0400

    Working on the demo.

--- src/emallocTest.c
+++ src/emallocTest.c
@@ -14,19 +14,68 @@ PBL_APP_INFO(MY_UUID,
 			 APP_INFO_STANDARD_APP);
 
 Window window;
+TextLayer tl1;
+TextLayer tl2;
+TextLayer dbg;
 
 struct emallocBufferInfo emb;
-
 char buffer[BUFF_SIZE];
+char dbgMsg[500];
+
+void dump_up(ClickRecognizerRef recognizer, Window *window) {
+	(void)recognizer;
+	(void)window;
+	
+	char* ptr = emalloc(emb, 30);
+	for (int i = 0; i < 29; ++i)
+		if(ptr[i] == 0)
+			ptr[i] = 'z';
+	ptr[29] = 0;
+	text_layer_set_text(&tl1, ptr);
+}
+
+void click_config_provider(ClickConfig **config, Window *window) {
+	(void)window;
+
+	config[BUTTON_ID_UP]->click.handler = (ClickHandler) dump_up;
+}
 
 void handle_init(AppContextRef ctx)
 {
 	window_init(&window, "Window Name");
 
 	setupEmallocBufferInfo(&emb, buffer, BUFF_SIZE);
-	char* ptr = emalloc(emb, 500);
-	char* ptr2 = ecalloc(emb, 1, 500);
-	ptr2 = erealloc(emb, ptr2, 600);
+	char* ptr = emalloc(emb, 5);
+	for (int i = 0; i < 4; ++i)
+		ptr[i] = 'a' + i;
+	ptr[4] = 0;
+
+	char* ptr2 = ecalloc(emb, 1, 5);
+	for (int i = 0; i < 4; ++i)
+		ptr2[i] = 'a' + i;
+	ptr2[4] = 0;
+
+	ptr = erealloc(emb, ptr, 10);
+	for (int i = 0; i < 9; ++i)
+		ptr[i] = 'a' + i;
+	ptr[9] = 0;
+
+	text_layer_init(&tl1, GRect(0,0,144,30));
+	text_layer_set_text(&tl1, ptr);
+	layer_add_child(&window.layer, &tl1.layer);
+
+	text_layer_init(&tl2, GRect(0,30,144,30));
+	text_layer_set_text(&tl2, ptr2);
+	layer_add_child(&window.layer, &tl2.layer);
+
+	snprintf(dbgMsg, 500, "%X\n%X\n%X\n", buffer, ptr, ptr2);
+
+	text_layer_init(&dbg, GRect(0,60,144,168));
+	text_layer_set_text(&dbg, dbgMsg);
+	layer_add_child(&window.layer, &dbg.layer);
+
+	window_set_click_config_provider(&window, (ClickConfigProvider) click_config_provider);
+
 	efree(emb, ptr);
 	efree(emb, ptr2);
 
